4b12a5b8a5c6267396d8615175800984e881942d
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-08-16 03:36:51 +0100

    Improve UX

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use skim::{Skim, SkimOptions};
+use skim::{Skim, SkimOptionsBuilder};
 use std::convert::TryFrom;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
@@ -18,7 +18,7 @@ fn parse_unicode_data_line(line: &str) -> Option<String> {
                     name.retain(|c| c != ',' && c != '<' && c != '>');
                     name = name.to_uppercase()
                 }
-                Some(format!("{:100} {}\n", name.to_lowercase(), character))
+                Some(format!("{:5} {}\n", character, name.to_lowercase()))
             } else {
                 None
             }
@@ -46,12 +46,19 @@ fn generate_unicode_table(input: &str) -> Result<String, std::io::Error> {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let mut options = SkimOptions::default();
-    options.regex = true;
+    let options = SkimOptionsBuilder::default()
+        .regex(true)
+        .reverse(true)
+        .height(Some("80%"))
+        .build()
+        .unwrap();
     let path = generate_unicode_table("./assets/UnicodeData.txt")?;
     let output = File::open(path)?;
     let reader = BufReader::new(output);
-
-    Skim::run_with(&options, Some(Box::new(reader)));
+    if let Some(output) = Skim::run_with(&options, Some(Box::new(reader))) {
+        for item in output.selected_items.iter() {
+            println!("{}", item.get_output_text());
+        }
+    }
     Ok(())
 }
