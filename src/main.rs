3c944b11a8e4a3518b972947b263b3a5c6426d79
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-08-16 05:42:14 +0100

    Allow configurability through CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,59 @@
 use skim::{Skim, SkimOptionsBuilder};
 use std::io::{BufReader, Error};
+use structopt::clap::arg_enum;
+use structopt::StructOpt;
+
+arg_enum! {
+    #[derive(PartialEq)]
+    enum Search {
+        Regex,
+        Exact,
+        Fuzzy
+    }
+}
+
+arg_enum! {
+    #[derive(PartialEq)]
+    enum Layout {
+        Above,
+        Below,
+    }
+}
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "fu",
+    about = "Find Unicode characters with ease. Simply type a description of the character you are looking for."
+)]
+struct Options {
+    #[structopt(
+        raw(possible_values = "&Search::variants()", case_insensitive = "true",),
+        long = "search",
+        help = "Search mode",
+        default_value = "Regex"
+    )]
+    search: Search,
+    #[structopt(
+        raw(possible_values = "&Layout::variants()", case_insensitive = "true",),
+        long = "layout",
+        help = "Position of fu's window relative to the prompt",
+        default_value = "Below"
+    )]
+    layout: Layout,
+    #[structopt(
+        long = "height",
+        help = "Height of fu's window relative to the terminal window",
+        default_value = "80%"
+    )]
+    height: String,
+}
 
 fn main() -> Result<(), Error> {
+    let options = Options::from_args();
     let options = SkimOptionsBuilder::default()
-        .regex(true)
-        .reverse(true)
-        .height(Some("80%"))
+        .regex(options.search == Search::Regex)
+        .reverse(options.layout == Layout::Below)
+        .height(Some(&options.height))
         .build()
         .unwrap();
     let unicode_data = BufReader::new(&include_bytes!("UnicodeData")[..]);
